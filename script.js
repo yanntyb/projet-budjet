67e6a4b72e903e46f1fdbe27c2da672345253c3f
yanntyb <69966391+yanntyb@users.noreply.github.com>
2021-01-04 16:14:24 +0100

    A

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let button = document.getElementById("calc");
 let reset = document.getElementById("reset");
+let print = document.getElementById("print");
+
 let modalActive = false;
 let somme = 0;
 
@@ -9,7 +11,7 @@ function checkButton(){
             somme = 0;
             modalActive = checkInputValue();
         }
-    })
+    });
     reset.addEventListener("mouseup",function (){
         somme = 0;
         let inputTab = document.getElementsByTagName("input");
@@ -18,7 +20,8 @@ function checkButton(){
             value.style.border = "none";
         }
         document.getElementById("resultatSumm").innerHTML = "";
-    })
+    });
+    print.addEventListener("mouseup",definePdf);
 }
 
 function checkInputValue(){
@@ -98,6 +101,60 @@ function showResult(){
         message = "Vous avez actuelement : "
     }
     div.innerHTML = message + "<br>" + parseFloat(somme) + "€";
+}
+
+function definePdf(){
+    let result = document.getElementById("resultatSumm").innerHTML
+    if(result !== ""){
+        let divGlobalDepense = document.getElementById("depense");
+        let sousDivDepense = divGlobalDepense.getElementsByClassName("calcule")[0];
+        let inputTab = sousDivDepense.getElementsByTagName("input");
+
+        let divGlobalRecette = document.getElementById("recette");
+        let sousDivRecette = divGlobalRecette.getElementsByClassName("calcule")[0];
+        let inputTabRecette = sousDivRecette.getElementsByTagName("input");
+
+        let divGloablEpargne = document.getElementById("epargne");
+        let sousDivEpargne = divGloablEpargne.getElementsByClassName("calcule")[0];
+        let inputTabEpargne = sousDivEpargne.getElementsByTagName("input");
+
+        let docDef = {
+            content: [
+                "Liste dépense :",
+                {
+                    ul: [
+                        inputTab[0].placeholder + " " + inputTab[0].value + "€",
+                        inputTab[1].placeholder + " " + inputTab[1].value + "€",
+                        inputTab[2].placeholder + " " + inputTab[2].value + "€",
+                        inputTab[3].placeholder + " " + inputTab[3].value + "€",
+                        inputTab[4].placeholder + " " + inputTab[4].value + "€",
+                        inputTab[5].placeholder + " " + inputTab[5].value + "€",
+                        inputTab[6].placeholder + " " + inputTab[6].value + "€",
+                        inputTab[7].placeholder + " " + inputTab[7].value + "€",
+                        inputTab[8].placeholder + " " + inputTab[8].value + "€",
+                        inputTab[9].placeholder + " " + inputTab[9].value + "€",
+                        inputTab[10].placeholder + " " + inputTab[10].value + "€",
+                        inputTab[11].placeholder + " " + inputTab[11].value + "€",
+                        inputTab[12].placeholder + " " + inputTab[12].value + "€",
+                    ]
+                },
+                " ",
+                "Liste gain :",
+                {
+                    ul:[
+                        inputTabRecette[0].placeholder + " " + inputTabRecette[0].value + "€",
+                        inputTabRecette[1].placeholder + " " + inputTabRecette[1].value + "€",
+                        inputTabRecette[2].placeholder + " " + inputTabRecette[2].value + "€",
+                        inputTabRecette[3].placeholder + " " + inputTabRecette[3].value + "€",
+                        inputTabEpargne[0].placeholder + " " + inputTabEpargne[0].value + "€",
+                    ]
+                },
+                " ",
+                "Total : " + somme + "€",
+            ]
+        };
+        pdfMake.createPdf(docDef).open();
+    }
 }
 
 checkButton();
